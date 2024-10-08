fb8e5313f82897eb9c5bf1131cedaaf5e813cfaf
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-10-08 21:16:38 +0000

    Code written to meet TOP requirements.

--- script/script.js
+++ script/script.js
@@ -22,53 +22,57 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     getHumanOption = prompt("Enter your option: ");
-    return getHumanOption;
-}
-
-let humanScore = 0
-let computerScore = 0
-
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    switch (true) {
-        case humanChoice === "rock" && computerChoice === "paper":
-        case humanChoice === "paper" && computerChoice === "scissor":
-        case humanChoice === "scissor" && computerChoice === "rock":
-            computerScore += 1;
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-            break;
-
-        case humanChoice === "rock" && computerChoice === "scissor":
-        case humanChoice === "paper" && computerChoice === "rock":
-        case humanChoice === "scissor" && computerChoice === "paper":
-            humanScore += 1;
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-            break;
-        
-        default:
-            console.log(`Draw! You choose ${humanChoice} and computer choose ${computerChoice}`);
-            break;
-    }
+    const choices = ["rock", "paper", "scissor"];
+        if (!choices.includes(getHumanOption.toLowerCase())) {
+            return "Invalid";
+        }
+    return getHumanOption.toLowerCase();
 }
 
 
 function playGame(numberOfRounds) {
-    let rounds = 0;
-    while (rounds < numberOfRounds) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-    rounds++;
+    let humanScore = 0
+    let computerScore = 0
+
+
+    function playRound(humanChoice, computerChoice) {
+        switch (true) {
+            case humanChoice === "rock" && computerChoice === "paper":
+            case humanChoice === "paper" && computerChoice === "scissor":
+            case humanChoice === "scissor" && computerChoice === "rock":
+            case humanChoice === "Invalid":
+                computerScore += 1;
+                console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+                break;
+
+            case humanChoice === "rock" && computerChoice === "scissor":
+            case humanChoice === "paper" && computerChoice === "rock":
+            case humanChoice === "scissor" && computerChoice === "paper":
+                humanScore += 1;
+                console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+                break;
+
+            default:
+                console.log(`Draw! You choose ${humanChoice} and computer choose ${computerChoice}`);
+                break;
+        }
+    }    
+
+    let round = 0;
+    while (round < numberOfRounds) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection)
+        round++
     }
-    
     const gameResult = humanScore > computerScore ? "You won!" :
     humanScore < computerScore ? "You lost!" : 
     "Draw!";
-
+    console.log("User score: " + humanScore);
+    console.log("Computer score: " + computerScore);    
     console.log("Game result: " + "\n" + gameResult);
-    
-}
 
-playGame(5)
+}
+    
+playGame(2)
